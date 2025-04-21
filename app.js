4b6e5131d2b9bba18b3c314f418ee469dc1ddab5
Skyagencia <skyagenciadgt@gmail.com>
2025-04-21 19:48:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    headless: false,
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   }
 });
